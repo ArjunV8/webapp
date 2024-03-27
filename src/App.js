4337676c32e70e82eb2c78d75827e9d8c1da723d
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-27 16:39:52 -0400

    username attempt

--- src/App.js
+++ src/App.js
@@ -16,18 +16,19 @@ import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { generateClient} from 'aws-amplify/api';
+import { generateClient } from 'aws-amplify/api';
 import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 import { Auth } from 'aws-amplify';
-
 
 const client = generateClient();
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
+  const [username, setUsername] = useState(null);
 
   useEffect(() => {
     fetchNotes();
+    fetchUsername(); // Call fetchUsername when the component mounts
   }, []);
 
   async function fetchNotes() {
@@ -36,7 +37,7 @@ const App = ({ signOut }) => {
     await Promise.all(
       notesFromAPI.map(async (note) => {
         if (note.image) {
-          const url = await getUrl({ key: note.name});
+          const url = await getUrl({ key: note.name });
           note.image = url.url;
         }
         return note;
@@ -87,7 +88,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1} color="red">Welcome back {username} </Heading>
+      <Heading level={1} color="red">Welcome back {username}</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
@@ -107,31 +108,30 @@ const App = ({ signOut }) => {
       <View margin="3rem 0">
         {notes.map((note) => (
           <Flex
-          key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${note.name}`} // Fixing the interpolation here
+                style={{ width: 400 }}
+              />
+            )}
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
           </Flex>
         ))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
-    
   );
 };
 
